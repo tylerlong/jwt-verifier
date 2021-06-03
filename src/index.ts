d5a7bcc51d0cbf0815ff12e918563aac969c0301
Tyler Long <tyler4long@gmail.com>
2021-06-03 13:41:18 -0700

    First runnable version

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import axios from 'axios';
+import {jwtVerify} from 'jose/jwt/verify';
+import parseJwk from 'jose/webcrypto/jwk/parse';
+
 import {KeyStore} from './types';
 
 console.log(process.env.jwks_uri);
@@ -7,4 +10,23 @@ console.log(process.env.jwks_uri);
   const r = await axios.get(process.env.jwks_uri!);
   const keyStore = r.data as KeyStore;
   console.log(keyStore);
+  const keys: any = {};
+  for (const key of keyStore.keys) {
+    keys[key.kid] = await parseJwk(key);
+  }
+
+  const {payload, protectedHeader} = await jwtVerify(
+    process.env.assertion!,
+    keys[process.env.assertion_kid!]
+  );
+
+  console.log(payload, protectedHeader);
 })();
+
+/*
+
+const assertion = await new SignJWT(jwtData.body)
+    .setProtectedHeader(jwtData.header)
+    .sign(await parseJwk(keys.privateKey1));
+
+*/
