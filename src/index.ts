24a5b690dce2fc43ebad2d0dbefae98eeafa3ec0
Tyler Liu <tyler4long@gmail.com>
2026-08-24 11:50:55 -0700

    root-level await is supported

--- src/index.ts
+++ src/index.ts
@@ -3,26 +3,24 @@ import * as jose from "jose";
 
 import keyStore from "./public-keys.json" with { type: "json" };
 
-(async () => {
-	const {
-		assertion,
-		assertion_kid: assertionKid,
-		client_assertion: clientAssertion,
-		client_assertion_kid: clientAssertionKid,
-	} = process.env;
-	assert(
-		assertion && assertionKid && clientAssertion && clientAssertionKid,
-		"Missing required credentials",
-	);
+const {
+	assertion,
+	assertion_kid: assertionKid,
+	client_assertion: clientAssertion,
+	client_assertion_kid: clientAssertionKid,
+} = process.env;
+assert(
+	assertion && assertionKid && clientAssertion && clientAssertionKid,
+	"Missing required credentials",
+);
 
-	const keys: Record<string, Awaited<ReturnType<typeof jose.importJWK>>> = {};
-	for (const key of keyStore.keys) {
-		keys[key.kid] = await jose.importJWK(key);
-	}
+const keys: Record<string, Awaited<ReturnType<typeof jose.importJWK>>> = {};
+for (const key of keyStore.keys) {
+	keys[key.kid] = await jose.importJWK(key);
+}
 
-	let result = await jose.jwtVerify(assertion, keys[assertionKid]);
-	console.log(result);
+let result = await jose.jwtVerify(assertion, keys[assertionKid]);
+console.log(result);
 
-	result = await jose.jwtVerify(clientAssertion, keys[clientAssertionKid]);
-	console.log(result);
-})();
+result = await jose.jwtVerify(clientAssertion, keys[clientAssertionKid]);
+console.log(result);
