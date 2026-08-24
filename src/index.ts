5d43f18032f099b94b1029ee224a374cf81b44e0
Tyler Liu <tyler4long@gmail.com>
2026-08-24 11:04:27 -0700

    Replace gts with biome

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
-import * as jose from 'jose';
+import * as jose from "jose";
 
-import keyStore from './public-keys.json';
+import keyStore from "./public-keys.json";
 
 (async () => {
-  const keys: {[kid: string]: jose.KeyLike} = {};
-  for (const key of keyStore.keys) {
-    keys[key.kid] = (await jose.importJWK(key)) as jose.KeyLike;
-  }
+	const keys: { [kid: string]: jose.KeyLike } = {};
+	for (const key of keyStore.keys) {
+		keys[key.kid] = (await jose.importJWK(key)) as jose.KeyLike;
+	}
 
-  let result = await jose.jwtVerify(
-    process.env.assertion!,
-    keys[process.env.assertion_kid!]
-  );
-  console.log(result);
+	let result = await jose.jwtVerify(
+		process.env.assertion!,
+		keys[process.env.assertion_kid!],
+	);
+	console.log(result);
 
-  result = await jose.jwtVerify(
-    process.env.client_assertion!,
-    keys[process.env.client_assertion_kid!]
-  );
-  console.log(result);
+	result = await jose.jwtVerify(
+		process.env.client_assertion!,
+		keys[process.env.client_assertion_kid!],
+	);
+	console.log(result);
 })();
