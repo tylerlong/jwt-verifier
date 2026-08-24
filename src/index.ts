654cb2c00769f5f4f7dcad175dee425d4ddd2f09
Tyler Liu <tyler4long@gmail.com>
2026-08-24 11:49:26 -0700

    Fix warnings

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,28 @@
+import assert from "node:assert/strict";
 import * as jose from "jose";
 
 import keyStore from "./public-keys.json" with { type: "json" };
 
 (async () => {
-	const keys: { [kid: string]: jose.KeyLike } = {};
+	const {
+		assertion,
+		assertion_kid: assertionKid,
+		client_assertion: clientAssertion,
+		client_assertion_kid: clientAssertionKid,
+	} = process.env;
+	assert(
+		assertion && assertionKid && clientAssertion && clientAssertionKid,
+		"Missing required credentials",
+	);
+
+	const keys: Record<string, Awaited<ReturnType<typeof jose.importJWK>>> = {};
 	for (const key of keyStore.keys) {
-		keys[key.kid] = (await jose.importJWK(key)) as jose.KeyLike;
+		keys[key.kid] = await jose.importJWK(key);
 	}
 
-	let result = await jose.jwtVerify(
-		process.env.assertion!,
-		keys[process.env.assertion_kid!],
-	);
+	let result = await jose.jwtVerify(assertion, keys[assertionKid]);
 	console.log(result);
 
-	result = await jose.jwtVerify(
-		process.env.client_assertion!,
-		keys[process.env.client_assertion_kid!],
-	);
+	result = await jose.jwtVerify(clientAssertion, keys[clientAssertionKid]);
 	console.log(result);
 })();
